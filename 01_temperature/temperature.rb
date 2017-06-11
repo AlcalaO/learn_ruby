21d29d6006096d08d90426f2f827f6263af11a10
AlcalaO <oswaldoodavidal@gmail.com>
2017-06-10 21:36:00 -0400

    Excercise complete

--- 01_temperature/temperature.rb
+++ 01_temperature/temperature.rb
@@ -1,1 +1,10 @@
 #write your code here
+def ftoc temp
+    celsius_temp = (temp - 32) * 5/9
+    return celsius_temp
+end
+
+def ctof temp
+    fahrenheit_temp = temp.to_f * 9 / 5 + 32
+    return fahrenheit_temp
+end
--- Gemfile
+++ Gemfile
@@ -2,3 +2,5 @@ source "https://rubygems.org"
 
 gem 'rake', '< 11.0'
 gem 'rspec', '~> 3.4'
+gem 'pry'
+gem 'rsense'
