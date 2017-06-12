46c6f151ed17c49a9dbbcc7c04cd30dcce4c3aa0
AlcalaO <oswaldoodavidal@gmail.com>
2017-06-12 18:23:43 -0400

    Working.

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -7,3 +7,17 @@ end
 def shout message
     return message.upcase
 end
+
+def repeat (message, times=1)
+    if times == 1
+        repeated_message = message + ' ' + message
+        return repeated_message
+    else
+        repeated_message = message
+        for i in 1..times
+            repeated_message.concat(' ' + message)
+        end
+
+        return repeated_message
+    end
+end
