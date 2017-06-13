2abbc3c33ed4dcd50eb5d5d3ae48828f4ac0369c
AlcalaO <oswaldoodavidal@gmail.com>
2017-06-12 20:17:31 -0400

    Simon says completed

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -25,3 +25,31 @@ end
 def start_of_word (message, count)
     return message.split('').first(count).join
 end
+
+def first_word message
+    return message.split(' ').first
+end
+
+def titleize message
+    if message.split(' ').size == 1
+        return message.capitalize
+    else
+        little_words = ['and', 'the', 'over']
+        capitalized_message = ''
+        n_word = message.split(' ').size
+        arr_message = message.split(' ')
+
+        for i in 0..n_word-1
+            if little_words.include?(arr_message[i]) && i == 0
+                capitalized_message.concat(arr_message[i].capitalize + ' ')
+            elsif little_words.include?(arr_message[i])
+                capitalized_message.concat(arr_message[i] + ' ')
+            else
+                capitalized_message.concat(arr_message[i].capitalize + ' ')
+            end
+        end
+
+        return capitalized_message.chop
+        
+    end
+end
