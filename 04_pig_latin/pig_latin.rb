94a2d6aac9c9a93f954748d2de5686f6888a01d9
AlcalaO <oswaldoodavidal@gmail.com>
2017-06-13 21:36:39 -0400

    Working

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,21 +1,25 @@
 #write your code here
 
 def translate (message)
-    vowels = 'aeiou'.split('')
-    consonants = 'qdrwbjfpshtgnzxmcvkl'.split('')
-    sound = 'ay'
-    new_message = message
-    arr_message = message.split('')
+  vowels = 'aeiou'.split('')
+  consonants = 'qdrwbjfpshtgnzxmcvkl'.split('')
+  sound = 'ay'
+  new_message = message
+  arr_message = message.split('')
 
-    if vowels.include?(message.split('').first)
-        new_message.concat(sound)
-        return new_message
-    elsif consonants.include?(message.split('').first)
-        arr_message.push(message.split('').first)
-        arr_message.push(sound)
-        arr_message.delete_at(0)
-        return arr_message.join
-    end
-    
+  if vowels.include?(arr_message.first)
+    new_message.concat(sound)
+    return new_message
+  elsif consonants.include?(arr_message.first)
+    arr_message.push(message.split('').first)
+    arr_message.push(sound)
+    arr_message.delete_at(0)
+    return arr_message.join
+  end
 
+end
+
+def find_n_consonants (word)
+  vowels = 'aeiou'.split('')
+  
 end
