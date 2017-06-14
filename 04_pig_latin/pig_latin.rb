81f3e9350ea985d785de7cc7de6fd81e3505e407
AlcalaO <oswaldoodavidal@gmail.com>
2017-06-13 21:24:22 -0400

    Working on pig_latin

--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,21 @@
 #write your code here
+
+def translate (message)
+    vowels = 'aeiou'.split('')
+    consonants = 'qdrwbjfpshtgnzxmcvkl'.split('')
+    sound = 'ay'
+    new_message = message
+    arr_message = message.split('')
+
+    if vowels.include?(message.split('').first)
+        new_message.concat(sound)
+        return new_message
+    elsif consonants.include?(message.split('').first)
+        arr_message.push(message.split('').first)
+        arr_message.push(sound)
+        arr_message.delete_at(0)
+        return arr_message.join
+    end
+    
+
+end
